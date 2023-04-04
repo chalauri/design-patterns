99da8b89ab984ffe25c563acbd97c7c1a84a342e
Giga Chalauri <gigachalauri@Gigas-MacBook-Air.local>
2023-04-04 08:23:49 +0200

    remove not needed code

--- src/main/java/ge/edu/sangu/decorator/component/Beverage.java
+++ src/main/java/ge/edu/sangu/decorator/component/Beverage.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 
 public interface Beverage {
 
-    public abstract String getDescription();
+    String getDescription();
 
-    public abstract BigDecimal getCost();
+    BigDecimal getCost();
 }
